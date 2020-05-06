e063af6117e5b7c7a8a60722cba4d67259f58fef
Sasidhar <sasidharpurum@gmail.com>
2020-05-07 02:23:15 +0530

    choice fuction comment in bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -13,7 +13,7 @@ months = ('january', 'february', 'march', 'april', 'may', 'june')
 weekdays = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
             'saturday')
 
-
+# choice function created 
 def choice(prompt, choices=('y', 'n')):
     """Return a valid input from the user given an array of possible answers.
     """
